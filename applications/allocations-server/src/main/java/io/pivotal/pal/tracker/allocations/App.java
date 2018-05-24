10f8dc492969bc56a7db847632f9993f3247d96a
pal_user <ace_14r@yahoo.co.in>
2018-05-24 15:06:55 -0600

    updated - OAuth checkins

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,6 +16,8 @@ import java.util.TimeZone;
 
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableResourceServer
 @EnableEurekaClient
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
 @EnableCircuitBreaker
